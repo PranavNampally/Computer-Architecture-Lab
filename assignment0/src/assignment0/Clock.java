2b1ea16a3292ae75ae6360f15d96f9582e6a4503
PranavNampally <pranav.nampally@gmail.com>
2021-08-12 22:28:54 +0530

    writing to csv

--- assignment0/src/assignment0/Clock.java
+++ assignment0/src/assignment0/Clock.java
@@ -1,55 +1,64 @@
 package assignment0;
 
 import java.util.Random;
+
+
 public class Clock {
 	
 	public static void main(String[] args) {
-		double probs[]= {0.1, 0.4, 0.6, 0.8, 0.9};
-		int widths[]= {5, 10, 15, 20, 25};
-		for(int prob_index=0; prob_index<5; prob_index++) {
-			for(int width_index=0; width_index<5; width_index++) {
-				double prob=probs[prob_index];
-				int width=widths[width_index];
-				
-				Border board=new Border(width);	
-				board.sensorArrInit(prob);
-				
-				Random rand = new Random();
-				int inf_x=rand.nextInt(1000);
-				Infiltrator natasha=new Infiltrator(inf_x);	
-				
-//				for (int i=0; i<board.w; i++) {				      Printing to debug
-//					for(int j=0; j<board.l; j++) {
-//						if(board.sensorArr[i][j].state==true) {
-//							System.out.print("1 ");
-//						}
-//						else {
-//							System.out.print("0 ");
-//						}
-//					}
-//					System.out.print("\n");
-//				}
-//				System.out.print(natasha.x + " " + natasha.y + "\n");
-				
-				int t=0;
-				while((natasha.caught==false) && (natasha.y<(board.w-1))) {
-					for (int i=0; i<board.w; i++) {				      
-						for(int j=0; j<board.l; j++) {
-							board.sensorArrUpdate(t, prob);
+		
+		fileop dataPlot = new fileop();
+		dataPlot.CreateFileDemo();
+		
+		
+		double prob_arr[]= {0.1, 0.3, 0.5, 0.7, 0.9};
+		int width_arr[]= {5, 10, 15, 20, 25};
+		for(int prob_index=0; prob_index<prob_arr.length; prob_index++) {
+			for(int width_index=0; width_index<width_arr.length; width_index++) {
+				int t_avg=0;
+				int AC_wins=0;
+				double prob=prob_arr[prob_index];
+				int width=width_arr[width_index];
+				for(int iter=0; iter<10; iter++) {
+					
+					Border board=new Border(width);	
+					board.sensorArrInit(prob);
+					
+					Random rand = new Random();
+					int inf_x=rand.nextInt(1000);
+					Infiltrator natasha=new Infiltrator(inf_x);	
+					
+					
+					int t=0;
+					while((natasha.caught==false) && (natasha.y<(board.w-1))) {
+						for (int i=0; i<board.w; i++) {				      
+							for(int j=0; j<board.l; j++) {
+								board.sensorArrUpdate(t, prob);
+							}
 						}
+						natasha.infiltratorMovement(t, board.l, prob);
+						if(board.sensorArr[natasha.y][natasha.x].state==true) {
+							natasha.caught=true;
+							System.out.println("time= "+t+" prob= "+prob+" w= "+board.w);
+							t_avg+=t;
+							break;
+						}
+						t+=10;
 					}
-					natasha.infiltratorMovement(t, board.l, prob);
-					if(board.sensorArr[natasha.y][natasha.x].state==true) {
-						natasha.caught=true;
-						System.out.println("Surprise MF"+" time= "+t+" prob= "+prob+" w= "+board.w);
-						break;
+					if(natasha.caught==false) {
+						System.out.println("time= "+t+" prob= "+prob+" w= "+board.w);
+						t_avg+=t;
+						AC_wins+=1;
 					}
-					t+=1;
 				}
-				if(natasha.caught==false) {
-					System.out.println("I have outsmarted your outsmartedness"+" time= "+t+" prob= "+prob+" w= "+board.w);
-				}
+				t_avg/=10;
+				System.out.println("time_avg= "+t_avg+" prob= "+prob+" w= "+width+" AC wins= "+AC_wins);
+				dataPlot.AppendFileDemo(t_avg,prob,width,AC_wins);
+				
+				
+
 			}
+				
 		}
 		
 		
---
+++ assignment0/src/assignment0/fileop.java
@@ -0,0 +1,62 @@
+package assignment0;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class fileop{
+void CreateFileDemo()
+{
+   	
+      try {
+	     File file = new File("D:\\Google Drive\\IITDH 5 sem\\Computer Architecture Lab\\Assignment 0\\data.csv");
+	     /*If file gets created then the createNewFile() 
+	      * method would return true or if the file is 
+	      * already present it would return false
+	      */
+             boolean fvar = file.createNewFile();
+	     if (fvar){
+	          System.out.println("File has been created successfully");
+	     }
+	     else{
+	          System.out.println("File already present at the specified location");
+	     }
+    	} catch (IOException e) {
+    		System.out.println("Exception Occurred:");
+	        e.printStackTrace();
+	  }
+   
+}
+void AppendFileDemo(int t, double prob, int w, int wins)
+{
+   	
+      try{
+    
+        //Specify the file name and path here
+    	File file =new File("D:\\Google Drive\\IITDH 5 sem\\Computer Architecture Lab\\Assignment 0\\data.csv");
+
+    	/* This logic is to create the file if the
+    	 * file is not already present
+    	 */
+    	if(!file.exists()){
+    	   file.createNewFile();
+    	}
+
+    	//Here true is to append the content to file
+    	FileWriter fw = new FileWriter(file,true);
+    	//BufferedWriter writer give better performance
+    	BufferedWriter bw = new BufferedWriter(fw);
+    	bw.write(t+","+prob+","+w+","+wins+"\n");
+    	//Closing BufferedWriter Stream
+//    	bw.close();
+
+	System.out.println("Data successfully appended at the end of file");
+
+      }catch(IOException ioe){
+         System.out.println("Exception occurred:");
+    	 ioe.printStackTrace();
+       }
+   
+}
+}
